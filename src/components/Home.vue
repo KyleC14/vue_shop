d2a9f0642fbd77da45ff02fad856dcdf6abb8d1e
KyleC14 <kylechenszu@foxmail.com>
2021-03-16 21:17:21 +0800

    function switch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      Header <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,4 +26,17 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.home-container {
+  height: 100%;
+}
+// element-ui中组件的名称就是其类名
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,13 +6,18 @@ import Vue from 'vue'
 // //导入弹框提示组件
 // import { Message } from 'element-ui'
 
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 //注册
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+
 //message需要全局挂载
 Vue.prototype.$message = Message;
 
