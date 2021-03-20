7b0242f6ebf0edd612092d0820d86b848cbc0846
KyleC14 <kylechenszu@foxmail.com>
2021-03-20 17:14:56 +0800

    user list function completed

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import './assets/css/global.css'
 import axios from 'axios'
 //配置请求根路劲
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//挂载拦截器 config为请求对象 拦截器作用于发送请求前做处理挂载授权token
+axios.interceptors.request.use(config => {
+  // console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  //最后必须return config
+  return config
+})
 //挂载到原型 使得每一个组件都能通过this访问到$http从而发起axios请求
 Vue.prototype.$http = axios;
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,7 +6,7 @@ import Vue from 'vue'
 // //导入弹框提示组件
 // import { Message } from 'element-ui'
 
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
 
 //注册
 Vue.use(Button)
@@ -17,7 +17,22 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 
 //message需要全局挂载
 Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox;
 
